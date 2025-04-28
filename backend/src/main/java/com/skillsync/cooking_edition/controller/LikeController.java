7c811105b777f3bfd0dbf18df7177b3f6d9b8215
Walallawita <162669294+Walallawita@users.noreply.github.com>
2025-04-28 12:01:13 +0530

    like and comments

--- backend/src/main/java/com/skillsync/cooking_edition/controller/LikeController.java
+++ backend/src/main/java/com/skillsync/cooking_edition/controller/LikeController.java
@@ -1,5 +1,4 @@
 package com.skillsync.cooking_edition.controller;
-
 import com.skillsync.cooking_edition.service.InteractionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
--- backend/src/main/java/com/skillsync/cooking_edition/model/Comment.java
+++ backend/src/main/java/com/skillsync/cooking_edition/model/Comment.java
@@ -1,5 +1,4 @@
 package com.skillsync.cooking_edition.model;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
